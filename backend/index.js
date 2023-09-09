d317ce3b9e5ba874e7d6ea911067c43eb09bd6d5
Valee9 <valentina.hormazabal@alumnos.ucn.cl>
2023-09-09 17:16:14 -0300

    metodo post

--- backend/index.js
+++ backend/index.js
@@ -4,21 +4,38 @@ const app = express();
 app.use(express.json());
 
 const users = [
-    {name: 'Gonzalo Acevedo', app:'a', tiempo:'100', dateReserve: '2022-01-23'},
-    {name: 'Carlos González', app:'b', tiempo:'50', dateReserve: '2021-04-28'},
-    {name: 'Valentina Hormazabal', app:'c', tiempo:'120', dateReserve: '2022-03-13'},
-    {name: 'José García', app:'a', tiempo:'300', dateReserve: '2022-02-18'},
-    {name: 'Sofía Fernández', app:'b', tiempo:'240', dateReserve: '2023-12-03'},
-    {name: 'Andrea Torres', app:'c', tiempo:'38', dateReserve: '2023-09-30'},
-    {name: 'Luis Sánchez', app:'a', tiempo:'89', dateReserve: '2023-08-21'},
-    {name: 'Daniel Ruíz', app:'b', tiempo:'345', dateReserve: '2022-11-16'},
-    {name: 'Sergio Mendoza', app:'c', tiempo:'276', dateReserve: '2021-02-12'},
-    {name: 'Natalia Rojas', app:'a', tiempo:'165', dateReserve: '2020-04-24'},
-    {name: 'Andrés Jiménez', app:'b', tiempo:'379', dateReserve: '2020-06-21'},
+    {id: 1, name: 'Gonzalo Acevedo', app:'a', time:'100', dateReserve: '2022-01-23'},
+    {id: 2, name: 'Carlos González', app:'b', time:'50', dateReserve: '2021-04-28'},
+    {id: 3, name: 'Valentina Hormazabal', app:'c', time:'120', dateReserve: '2022-03-13'},
+    {id: 4, name: 'José García', app:'a', time:'300', dateReserve: '2022-02-18'},
+    {id: 5, name: 'Sofía Fernández', app:'b', time:'240', dateReserve: '2023-12-03'},
+    {id: 6, name: 'Andrea Torres', app:'c', time:'38', dateReserve: '2023-09-30'},
+    {id: 7, name: 'Luis Sánchez', app:'a', time:'89', dateReserve: '2023-08-21'},
+    {id: 8, name: 'Daniel Ruíz', app:'b', time:'345', dateReserve: '2022-11-16'},
+    {id: 9, name: 'Sergio Mendoza', app:'c', time:'276', dateReserve: '2021-02-12'},
+    {id: 10, name: 'Natalia Rojas', app:'a', time:'165', dateReserve: '2020-04-24'}
 ];
 
 app.get('/', (req,res) => {
     res.send('Node JS API Vale');
+});
+
+app.get('/api/users', (req,res) => {
+    res.send(users);
+    console.log(users)
+});
+
+app.post('/api/products' , (req,res) => {
+    const user = {
+        id: users.length + 1,
+        name: req.body.name,
+        app: req.body.app,
+        time: req.body.time,
+        dateReserve: req.body.dateReserve,
+    };
+
+    products.push(user);
+    res.send(user);
 });
 
 const port = process.env.port || 80;
--- prueba/src/App.js
+++ prueba/src/App.js
@@ -1,39 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
-function App() {
+const App = () => {
+  const url = `http://localhost:80/api/users`;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => { getData() }, []);
+
+  const getData = async () => {
+    const res = await axios.get(url);
+    setUsers(res.data)
+    console.log('resultados: ',users);
+  }
+
   return (
     <div> 
-      <div style={{fontSize: '2rem'}}>
-        INAISO
-      </div>
-      
-      <div>
-        <table className="table table-bordered table-striped table-hover w-75 ms-5 mt-2">
-          <thead className="thead thead-dark">
-            <tr>
-              <th>Número</th>
-              <th>Usuario</th>
-              <th>Reserva</th>
-              <th>Acciones</th>
-            </tr>
-          </thead>
-          <tbody id='body'>
-              <tr>
-                <td>1</td>
-                <td>lala</td>
-                <td>nana</td>
-                <td>
-                  <button className="btn btn-warning me-2">Editar</button>
-                  <button className="btn btn-danger">Eliminar</button>
-                </td>
-              </tr>
-          </tbody>
-        </table>
-      </div>
+    <div style={{fontSize: '2rem'}}>
+      INAISO
     </div>
-  );
+    
+    <div>
+      <table className="table table-bordered table-striped table-hover w-75 ms-5 mt-2">
+        <thead className="thead thead-dark">
+          <tr>
+            <th>Número</th>
+            <th>Usuario</th>
+            <th>Reserva</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody id='body'>
+            <tr>
+              <td>1</td>
+              <td>lala</td>
+              <td>nana</td>
+              <td>
+                <button className="btn btn-warning me-2">Editar</button>
+                <button className="btn btn-danger">Eliminar</button>
+              </td>
+            </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  )
 }
 
-export default App;
+export default App
